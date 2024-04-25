a8e31a13ecf7240db0b4b0423b1593d469ddb905
Sourabh Singh Rajput <sourabhr684@gmail.com>
2024-04-25 14:11:54 +0530

    Basic Inheritence Done in JAVA

---
+++ Week 3/Inheritence/demo.java
@@ -0,0 +1,11 @@
+public class demo{
+    public static void main(String[] args) {
+        advCalc obj= new advCalc();
+
+        System.out.println(obj.add(10,29));
+        System.out.println(obj.sub(10,29));
+        System.out.println(obj.mul(10,29));
+        System.out.println(obj.div(10,29));
+
+    }
+}
